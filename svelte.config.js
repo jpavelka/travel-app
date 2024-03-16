7a2ea42670521158c0a9b190c427a1cf2c765869
jpavelka <jeffp171@gmail.com>
2024-03-16 01:50:09 +0000

    workflow fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? "/travel-app" : "",
+        },
+	},
+	preprocess: preprocess()
 };
 
 export default config;
