e7d2bceaa50cf199c2961f0a5084655d12651f7a
jpavelka <jeffp171@gmail.com>
2024-03-17 13:48:29 +0000

    fix localstorage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,13 @@
   import LeafletMap from "$lib/LeafletMap.svelte";
   import Weather from "../lib/Weather.svelte";
   import WeatherWarning from "../lib/WeatherWarning.svelte";
-  import { browser } from "$app/environment"
-  $: whichToShow = "map";
+  import { browser } from "$app/environment";
+  let whichToShowInit = "map";
   if (browser) {
-    whichToShow = localStorage.getItem("tab");
+    whichToShowInit = localStorage.getItem("tab");
   }
+  $: whichToShow = whichToShowInit;
+  // whichToShow = "weather";
   const tabClick = (x) => {
     whichToShow = x;
     if (browser) {
@@ -16,30 +18,34 @@
 </script>
 
 <main>
-  <div style="display: flex">
-    <div
-      class={"tab " +
-        (whichToShow === "map" ? "tabSelected" : "tabNotSelected")}
-      on:click={() => tabClick("map")}
-    >
-      Map
+  {#if browser}
+    <div style="display: flex">
+      <div
+        class={"tab " +
+          (whichToShow === "map" ? "tabSelected" : "tabNotSelected")}
+        on:click={() => tabClick("map")}
+      >
+        Map
+      </div>
+      <div
+        class={"tab " +
+          (whichToShow === "weather" ? "tabSelected" : "tabNotSelected")}
+        on:click={() => tabClick("weather")}
+      >
+        Weather
+      </div>
+      <div style="border-bottom: 1pt solid black; width: 100%"></div>
     </div>
-    <div
-      class={"tab " +
-        (whichToShow === "weather" ? "tabSelected" : "tabNotSelected")}
-      on:click={() => tabClick("weather")}
-    >
-      Weather
-    </div>
-    <div style="border-bottom: 1pt solid black; width: 100%"></div>
-  </div>
-  <WeatherWarning />
-  {#if whichToShow === "map"}
-    <div style="padding: 5pt">
-      <LeafletMap />
-    </div>
-  {:else if whichToShow === "weather"}
-    <Weather />
+    <WeatherWarning />
+    {#if whichToShow === "map"}
+      <div style="padding: 5pt">
+        <LeafletMap />
+      </div>
+    {:else if whichToShow === "weather"}
+      <Weather />
+    {/if}
+  {:else}
+    Loading...
   {/if}
 </main>
 
