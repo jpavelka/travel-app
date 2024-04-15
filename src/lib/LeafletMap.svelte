b528ffc4d2105a02aaa43ca2748ee4174c445fd8
jpavelka <jeffp171@gmail.com>
2024-04-15 16:36:04 +0000

    add points of interest

--- src/lib/LeafletMap.svelte
+++ src/lib/LeafletMap.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import { browser } from "$app/environment";
-  import { tripData, diversionData, getDateData } from "$lib/getTripData.js";
+  import { tripData, diversionData, pointOfInterestData, getDateData } from "$lib/getTripData.js";
   import { whichToShow, placesInd, mapShown } from "$lib/stores.js";
 
   const dateData = getDateData(new Date());
@@ -68,6 +68,24 @@
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
         shadowSize: [41, 41],
+      });
+      const redIconSmall = new L.Icon({
+        iconUrl:
+          "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+        shadowUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+        iconSize: [18, 30],
+        iconAnchor: [9, 30],
+        popupAnchor: [1, -25],
+        shadowSize: [30, 30]
+      });
+      const invisibleIcon = new L.Icon({
+        iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png",
+        shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+        iconSize: [0, 0],
+        iconAnchor: [0, 0],
+        popupAnchor: [0, 0],
+        shadowSize: [0, 0]
       });
 
       for (let i in $tripData) {
@@ -136,6 +154,88 @@
           clickLine.addTo(map);
         }
       }
+      
+      let pointsOfInterest = [];
+      let pointsOfInterestLines = [];
+      let pointsOfInterestClickLines = [];
+      const poiLineWeight = 5;
+      const poiClickLineWeight = 30;
+      for (let i in $pointOfInterestData) {
+        i = parseInt(i);
+        const d = $pointOfInterestData[i];
+        let opts = { icon: redIconSmall };
+        const txtMrk = L.marker([d.lat, d.lng], { opacity: 0 });
+        txtMrk.bindTooltip(
+          d.date.toLocaleDateString().split("/").slice(0, 2).join("/"),
+          { permanent: true, className: "date-label", offset: [1, 0] },
+        );
+        txtMrk.addTo(map);
+        const mrk = L.marker([d.lat, d.lng], opts);
+        mrk.bindPopup(
+          [
+            `<b>${d.activity}</b>`,
+            `${d.campground}`,
+            `${d.city}`,
+          ].filter(x => x.length > 0).join("<br>"),
+        );
+        mrk.addTo(map);
+        pointsOfInterest.push(mrk);
+        const lineColor = "#d23";
+        const parent = d.parentType === 'campground' ? $tripData[d.parentInd] : $diversionData[d.parentInd];
+        const poiLine = L.polyline(
+          [
+            [parent.lat, parent.lng],
+            [d.lat, d.lng],
+          ],
+          { color: lineColor, weight: poiLineWeight, dashArray: "5,15" },
+        );
+        poiLine.addTo(map);
+        const poiClickLine = L.polyline(
+          [
+            [parent.lat, parent.lng],
+            [d.lat, d.lng],
+          ],
+          { opacity: 0, weight: poiClickLineWeight },
+        );
+        poiClickLine.bindPopup(
+          [
+            "<b>Point of Interest</b>",
+            `From: ${parent.campground} (${parent.city})`,
+            `To: ${d.campground} (${d.city})`,
+            `Map: <a href='https://www.google.com/maps/dir/${parent.lat},${parent.lng}/${d.lat},${d.lng}/@${d.lat},${d.lng}' target='_blank'>link</a>`,
+          ].join("<br>"),
+        );
+        poiClickLine.addTo(map);
+        pointsOfInterestLines.push(poiLine);
+        pointsOfInterestClickLines.push(poiClickLine);
+      }
+      
+      map.on('zoomend', function() {
+        const zoomLvl = 7;
+        var currentZoom = map.getZoom();
+        for (const poi of pointsOfInterest) {
+          if (currentZoom < zoomLvl) {
+            poi.setIcon(invisibleIcon)
+          } else {
+            poi.setIcon(redIconSmall)
+          }
+        }
+        for (const poiLine of pointsOfInterestLines) {
+          if (currentZoom < zoomLvl) {
+            poiLine.setStyle({weight: 0});
+          } else {
+            poiLine.setStyle({weight: poiLineWeight});
+          }
+        }
+        for (const poiClickLine of pointsOfInterestClickLines) {
+          if (currentZoom < zoomLvl) {
+            poiClickLine.setStyle({weight: 0});
+          } else {
+            poiClickLine.setStyle({weight: poiClickLineWeight});
+          }
+        }
+    });
+
       for (let i in $diversionData) {
         i = parseInt(i);
         const d = $diversionData[i];
